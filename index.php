555b5bf1763374f6ef44a2e69bf1f38ec4c3e504
WesleyHaring <D284479@edu.rocwb.nl>
2021-06-07 11:38:02 +0200

    begin attractie park

--- index.php
+++ index.php
@@ -25,7 +25,57 @@ require_once 'admin/backend/config.php';
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia modi dolore magnam! Iste libero voluptatum autem, sapiente ullam earum nostrum sed magnam vel laboriosam quibusdam, officia, esse vitae dignissimos nulla?
         </aside>
         <main>
-            <!-- hier komen de attractiekaartjes -->
+            <div class="attracties">
+                <div class="attractie">
+                    <img src="img/attracties/adger-kang-oiyzr-SgjBY-unsplash.jpg" alt="">
+                    <div class="head-attractie">
+                        <p>familyland</p>
+                        <h2>Reuzenrad</h2>
+                    </div>
+                    <p>Bekijk het park van grote hoogte met het hele gezin.</p>
+                    <div class="minimum-length">
+                    <p> ... minimale lengte</p>
+                    </div>
+                </div>
+                <div class="attractie fastpass">
+                    <img src="img/attracties/david-murcia-HbYniDwjbVE-unsplash.jpg" alt="">
+                    <div class="fastpass-text">   
+                        <div class="head-attractie">
+                            <p>adventureland</p>
+                            <h2>Thunderbolt</h2>
+                        </div>
+                        <p>Echte thrillseekers komen aan hun trekken.</p>
+                        <div class="minimum-length">
+                            <p> ... minimale lengte</p>
+                        </div>
+                        <div class="fastpass-button">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="attractie">
+                    <img src="img/attracties/alex-kalinin-6gYjwD4s9xk-unsplash.jpg" alt="">
+                    <div class="head-attractie">
+                        <p>familyland</p>
+                        <h2>Caroussel</h2>
+                    </div>
+                    <p>Voor de allerkleinsten: maak een rondje in de antieke draaimolen.</p>
+                    <div class="minimum-length">
+                    <p> ... minimale lengte</p>
+                    </div>
+                </div>
+                <div class="attractie">
+                    <img src="img/attracties/brandon-hoogenboom-P0MX2XCqbFc-unsplash.jpg" alt="">
+                    <div class="head-attractie">
+                        <p>waterland</p>
+                        <h2>Reuzeglijbaan</h2>
+                    </div>
+                    <p>Afkoelen in het waterparadijs (alleen open op warme dagen).</p>
+                    <div class="minimum-length">
+                    <p> ... minimale lengte</p>
+                    </div>
+                </div>
+            </div>
         </main>
     </div>
 
